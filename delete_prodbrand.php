e9e8ae9ba43e0d3468524aad2d6d372a6aba1c12
Hassan Sarfraz <Hassan Sarfraz@192.168.10.4>
2016-11-28 20:18:40 +0500

    new commit2

--- delete_prodbrand.php
+++ delete_prodbrand.php
@@ -1,13 +1,12 @@
 <?php
 include 'connection.php';
-$pbid = $_REQUEST['a'];
-$query = "delete from productbrand where pbid =$pbid";
 $result = mysqli_query($link, $query);
 if($result)
 {
     header("location:home.php?pg=disprob");
+    header("location:home.php");
 }
  else {
-    header("location:home.php?pg=disprob");   
-}
+    return 0;
+ }
 ?>
